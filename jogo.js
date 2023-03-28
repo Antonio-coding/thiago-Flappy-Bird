185781158a3487d1715f492eb0008d3332adc50f
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2023-03-28 11:38:31 -0300

    add gravidade
    adicionei o sistema de gravidade ao flappyBird

--- jogo.js
+++ jogo.js
@@ -1,95 +1,117 @@
-console.log('[Thiago] Flappy Bird');
+console.log("[Thiago] Flappy Bird");
 
 const sprites = new Image();
-sprites.src = './imagens.sprite.png';
+sprites.src = "./imagens.sprite.png";
 
-const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const contexto = canvas.getContext("2d");
 
 //[flappyBird]
 const flappyBird = {
-spriteX: 33,
-spriteY: 806,
-largura:33,
-altura:24,
-x:10,
-y:50,
-
-desenho(){
+  spriteX: 33,
+  spriteY: 806,
+  largura: 33,
+  altura: 24,
+  x: 10,
+  y: 50,
+  gravidade: 0.25,
+  velocidade: 0,
+  atualiza() {
+    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade;
+    flappyBird.y = flappyBird.y + flappyBird.velocidade;
+  },
+  desenho() {
     contexto.drawImage(
-        sprites,
-        flappyBird.spriteX,flappyBird.spriteY,
-        flappyBird.largura,flappyBird.altura,
-        flappyBird.x,flappyBird.y,
-        flappyBird.largura,flappyBird.altura,
+      sprites,
+      flappyBird.spriteX,
+      flappyBird.spriteY,
+      flappyBird.largura,
+      flappyBird.altura,
+      flappyBird.x,
+      flappyBird.y,
+      flappyBird.largura,
+      flappyBird.altura
     );
-
-}
-
-}
+  },
+};
 
 //[chao]
 const chao = {
-spriteX: 485,
-spriteY: 39,
-largura:263,
-altura:89,
-x:-1,
-y:393,
+  spriteX: 486,
+  spriteY: 39,
+  largura: 263,
+  altura: 89,
+  x: -0,
+  y: 393,
 
-desenho(){
+  desenho() {
     contexto.drawImage(
-        sprites,
-        chao.spriteX,chao.spriteY,
-        chao.largura,chao.altura,
-        chao.x,chao.y,
-        chao.largura,chao.altura,
+      sprites,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x,
+      chao.y,
+      chao.largura,
+      chao.altura
     );
     contexto.drawImage(
-        sprites,
-        chao.spriteX,chao.spriteY,
-        chao.largura,chao.altura,
-        (chao.x+chao.largura),chao.y,
-        chao.largura,chao.altura,
+      sprites,
+      chao.spriteX,
+      chao.spriteY,
+      chao.largura,
+      chao.altura,
+      chao.x + chao.largura,
+      chao.y,
+      chao.largura,
+      chao.altura
     );
-
-}
-
-}
+  },
+};
 
 //[plano de fundo]
 const planoDeFundo = {
-spriteX: 30,
-spriteY: 40,
-largura: 225,
-altura: 400,
-x: 0,
-y: 0,
+  spriteX: 30,
+  spriteY: 40,
+  largura: 220,
+  altura: 400,
+  x: 0,
+  y: 0,
 
-desenho(){
+  desenho() {
     contexto.drawImage(
-        sprites,
-        planoDeFundo.spriteX,planoDeFundo.spriteY,
-        planoDeFundo.largura,planoDeFundo.altura,
-        planoDeFundo.x,planoDeFundo.y,
-        planoDeFundo.largura,planoDeFundo.altura,
+      sprites,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
     );
     contexto.drawImage(
-        sprites,
-        planoDeFundo.spriteX,planoDeFundo.spriteY,
-        planoDeFundo.largura,planoDeFundo.altura,
-        (planoDeFundo.x + planoDeFundo.largura),planoDeFundo.y,
-        planoDeFundo.largura,planoDeFundo.altura,
+      sprites,
+      planoDeFundo.spriteX,
+      planoDeFundo.spriteY,
+      planoDeFundo.largura,
+      planoDeFundo.altura,
+      planoDeFundo.x + planoDeFundo.largura,
+      planoDeFundo.y,
+      planoDeFundo.largura,
+      planoDeFundo.altura
     );
+  },
+};
+function loop() {
+  planoDeFundo.desenho();
+  chao.desenho();
+  flappyBird.desenho();
 
-}
+  flappyBird.atualiza();
 
-}
-function loop(){
-    planoDeFundo.desenho();
-    chao.desenho();
-    flappyBird.desenho();
-    requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 loop();
